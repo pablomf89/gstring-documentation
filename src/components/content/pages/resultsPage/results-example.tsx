0119021593b9482cd5714451ade0c8693a499def
pablomf89 <53598745+pablomf89@users.noreply.github.com>
2022-12-15 17:21:40 +0100

    add results example

--- src/components/content/pages/resultsPage/results-example.tsx
+++ src/components/content/pages/resultsPage/results-example.tsx
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import { SelectInput } from "@3ffect/gstring";
 import { Results } from "@3ffect/gstring";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import { mockAPICall } from "./results-example-service";
 
 const columnsExample = [
   { title: "Name", columnKey: "title", sortable: true, filterOptions: [] },
@@ -9,23 +11,9 @@ const columnsExample = [
   { title: "Created on", columnKey: "createdOn", sortable: true, filterOptions: [], type: "date" },
 ];
 
-class ResultsExample extends Component<any> {
+class ResultsExample extends Component<RouteComponentProps> {
   fetchData = async (filters: any) => {
-    //in this function you'd usually call the backend to retrieve some data and pass the filters and the shortby
-    const _mock_data = [
-      { title: "result 1", categoryId: "Ipsum", createdByUser: "Pablo Martín", createdOn: "01/01/2022" },
-      { title: "result 2", categoryId: "Ipsum", createdByUser: "John Snow", createdOn: "02/01/2022" },
-      { title: "result 3", categoryId: "Ipsum", createdByUser: "Frodo", createdOn: "03/01/2022" },
-      { title: "result 4", categoryId: "Lorem", createdByUser: "El Risitas", createdOn: "04/01/2022" },
-      { title: "result 5", categoryId: "Dolor", createdByUser: "La Veneno", createdOn: "05/01/2022" },
-    ];
-    //here we're just mocking a simple category filter
-    const data = _mock_data.filter((item) => !filters.categoryId || item.categoryId === filters.categoryId);
-    //this would be the response from the backend
-    return {
-      count: data.length,
-      data: data,
-    };
+    return await mockAPICall(filters);
   };
 
   render() {
@@ -33,9 +21,9 @@ class ResultsExample extends Component<any> {
       <div className="g-item_16 g-item-tablet_8 g-item-mobile_4 mb8">
         <h3 className="hero mb2">Results</h3>
         {/* Please check the results PROPS interface for extra options. */}
-        <Results clientFiltering={false} filterOnChange={true} columns={columnsExample} defaultColumns={columnsExample} fetchResults={this.fetchData}>
+        <Results reactRouterHistory={this.props.history} clientFiltering={false} filterOnChange={true} columns={columnsExample} itemsPerPage={[2, 5, 10]} defaultColumns={columnsExample} fetchResults={this.fetchData}>
           {/* In this area you can add anything you want, it will appear on top of the table.
-          If you add form INPUTS they'll be automatically serialize and sent to fetchResults function as the filters. */}
+          If you add form INPUTS theyll be automatically serialize and sent to fetchResults function as the filters. */}
           <SelectInput label="Sample category filter" name="categoryId">
             <option value="">Select a category</option>
             <option value="Ipsum">Ipsum</option>
@@ -48,4 +36,4 @@ class ResultsExample extends Component<any> {
   }
 }
 
-export default ResultsExample;
+export default withRouter(ResultsExample);
